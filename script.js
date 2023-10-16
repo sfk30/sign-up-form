86a44bdad0eef817a3542444bca2149309e02931
Shafia <111538877+sfk30@users.noreply.github.com>
2023-10-16 12:04:26 +0100

    Reduced code

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ const error = document.getElementById('error')
 const submit_btn = document.getElementById('submit-btn')
 
 submit_btn.addEventListener('click', (e) => {
-    let messages = []
     if (password.value != confirm_password.value) {
         e.preventDefault()
-        messages.push('*Passwords do not match')
-        error.innerText = messages
+        error.innerText = '*Passwords do not match'
     }
 })
